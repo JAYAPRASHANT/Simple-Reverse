49195c09be5df89cdd4a1b0921275e29d53cba5e
JAYAPRASHANT <jayaprashanth.20eee@gmail.com>
2023-07-19 06:01:55 -0700

    prgm

---
+++ Simplereverse.java
@@ -0,0 +1,21 @@
+package SimpleReverse;
+import java.util.*;
+public class Simplereverse {
+     public static int Ss(String a) {
+    	 String rev="";
+    	 for(int i=0;i<a.length();i++) {
+    		 rev=a.charAt(i)+rev;
+    	 }
+		System.out.println(rev);
+		return 0;
+     }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+	    String a=sc.next();
+	    Ss(a);
+	    
+
+	}
+
+}
